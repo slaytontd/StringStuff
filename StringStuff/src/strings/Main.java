0bd3304ea076515b35c88db35319a0ff5ca3564d
slaytotd <slaytotd@LCBLAB2110-44.uccob.uc.edu>
2019-12-03 09:12:25 -0500

    end of class

--- StringStuff/src/strings/Main.java
+++ StringStuff/src/strings/Main.java
@@ -14,7 +14,11 @@ public class Main {
 		String test = "Todd Lindemann";
 		long startTime = System.nanoTime();
 		for (int i = 0; i < 1000000000; i++) {
-
+			//test = "Oscar Robertson"; //init a string variable with a string literal
+			//test = test + "!"; //append operation, but actually making a new string
+			String fruit = "Applee";
+			fruit = fruit.replace("ee", "e");
+			//System.out.println(fruit);
 		}
 		long endTime = System.nanoTime();
 
@@ -29,7 +33,14 @@ public class Main {
 		StringBuilder test = new StringBuilder("Todd Lindemann");
 		long startTime = System.nanoTime();
 		for (int i = 0; i < 1000000000; i++) {
-
+			//test = new StringBuilder("Oscar Robertson"); // same as above 
+			//test = test + "!"; 
+			//append stringbuilder with append operation 
+			//test.append("!"); 
+			test = new StringBuilder ("Applee");
+			//System.out.println("Before replace, test = " + test.toString());
+			test.replace(4,7,"e");//replaces 'ee' with 'e'
+			//System.out.println("After replace, test = " + test.toString());
 		}
 		long endTime = System.nanoTime();
 
